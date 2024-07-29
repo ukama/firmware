96b8fbc1a780788141bde7fc85ca878f8b426213
vthakur7f <vthakur7@hotmail.com>
2024-07-29 13:20:52 -0700

    U-boot config for partitioning and upgrades.
    Signed-off-by: vthakur7f <vthakur7@hotmail.com>

--- include/env_default.h
+++ include/env_default.h
@@ -106,6 +106,7 @@ const char default_environment[] = {
 #if defined(CONFIG_BOOTCOUNT_BOOTLIMIT) && (CONFIG_BOOTCOUNT_BOOTLIMIT > 0)
 	"bootlimit="	__stringify(CONFIG_BOOTCOUNT_BOOTLIMIT)"\0"
 #endif
+    "justtest=" __stringify(CONFIG_BOOTCOUNT_BOOTLIMIT)"\0"
 #ifdef CONFIG_MTDIDS_DEFAULT
 	 "mtdids="	CONFIG_MTDIDS_DEFAULT		"\0"
 #endif
